997af2b7c3cdd77fe4d70d9cfbca844df5db0bc6
amillert <albertmillert97@gmail.com>
2021-02-16 00:46:55 +0100

    ddp.yml to recreate conda env; setup.py to build project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='DialogueDiscourseParsing',
+    version='',
+    packages=['libs'],
+    url='',
+    license='',
+    author='amillert',
+    author_email='',
+    description=''
+)
